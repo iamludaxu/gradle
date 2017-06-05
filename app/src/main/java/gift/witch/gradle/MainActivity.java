46abf94ad2a214218e8706967cbfa4fc237b131a
iamludaxu <iamludaxu@163.com>
2017-06-05 21:22:54 +0800

    1.增加library 2.描述grade 3.增加界面显示内容

--- app/src/main/java/gift/witch/gradle/MainActivity.java
+++ app/src/main/java/gift/witch/gradle/MainActivity.java
@@ -2,6 +2,7 @@ package gift.witch.gradle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView appIDTV = (TextView) findViewById(R.id.appid);
+        TextView versionCodeTV = (TextView) findViewById(R.id.versionCode);
+        TextView versionNameTV = (TextView) findViewById(R.id.versionName);
+        TextView packageNameTV = (TextView) findViewById(R.id.packageName);
+
+        try {
+            String pkName = this.getPackageName();
+            String versionName = this.getPackageManager().getPackageInfo(
+                    pkName, 0).versionName;
+            int versionCode = this.getPackageManager()
+                    .getPackageInfo(pkName, 0).versionCode;
+            versionCodeTV.setText(String.valueOf(versionCode));
+            versionNameTV.setText(versionName);
+            packageNameTV.setText(pkName);
+        } catch (Exception e) {
+        }
+
+
     }
 }
